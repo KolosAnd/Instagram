ec214f18aaa4efebb8f9a5c8ace7c92441ceae28
Kolos_And <Andrewkolos520@gmail.com>
2020-03-31 22:14:41 +0300

    backend 2.0
    json-server, make render all posts from posts.json

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import InstaService from "../services/instaService";
+import User from "./User";
 
 
 export default class Posts extends Component {
@@ -7,13 +8,61 @@ export default class Posts extends Component {
     InstaService = new InstaService();
     state = {
         posts: [],
-        error: false  
+        error: false
+    };
+
+    componentDidMount() {
+        this.updatePost();
+    }
+
+    updatePost() {
+        this.InstaService.getAllPosts()
+            .then(this.onPostLoaded)
+            .catch(this.onError)
+    }
+
+    onPostLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        });
+        console.log(this.state.posts);
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    };
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const {name, altname, photo, src, alt, descr, id} = item;
+            return (
+                <div key={id} className="post">
+                    <User src={photo}
+                          alt={altname}
+                          name={name}
+
+                    />
+                    <img src={src} alt={alt}/>
+                    <div className="post__name">
+                        {name}
+                    </div>
+                    <div className="post__descr">
+                        {descr
+                        }</div>
+                </div>
+            )
+        })
     }
 
     render() {
+        const {error,posts} = this.state;
+        const items = this.renderItems(posts);
         return (
             <div className="left">
-
+                {items}
             </div>
         )
     }
